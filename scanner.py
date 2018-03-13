ee0b2406fa3c0fda2b867184d1db9c8c1ff60040
Orestis Kaplanis <orestismail71@gmail.com>
2018-03-13 16:19:59 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,4 +1,11 @@
 
+
+#Apodekto inputs 
+def print_input_forms():
+	"""print the acceptable inputs"""
+	print("The acceptable inputs are : \n HH:MM or HH.MM where H can be\n A: a number from 00-23 and  MM from 00-59   "
+	"\nand \nB: H.MM where H can be from 0-23 and MM 00-59  ")
+
 def getchar(words,pos):
 	""" returns char at pos of words, or None if out of bounds """
 
@@ -16,6 +23,8 @@ def scan(text,transition_table,accept_states):
 	# initial state
 	pos = 0
 	state = 'q0'
+	
+	
 	
 	while True:
 		
@@ -35,29 +44,26 @@ def scan(text,transition_table,accept_states):
 			# current state is not accepting
 			return 'ERROR_TOKEN',pos
 			
-	
-# the transition table, as a dictionary
 
-# Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων...
-td = { 'q0':{ 't':'q1','l':'q2' },
-       'q1':{ 'e':'q3' },
-       'q2':{ 'o':'q8' },
-       'q3':{ 's':'q4','r':'q6' },
-       'q4':{ 't':'q5' },
-       'q6':{ 'm':'q7' },
-       'q8':{ 'n':'q9' },
-       'q9':{ 'g':'q10'}
+# the transition table, as a dictionary
+td = { 'q0':{ '0':'q1', '1':'q3', '2':'q3', '3':'q4', '4':'q4', '5':'q4', '6':'q4', '7':'q4', '8':'q4', '9':'q4'},
+      'q1':{'0':'q2','1':'q2','2':'q2','3':'q2','4':'q2','5':'q2','6':'q2','7':'q2','8':'q2','9':'q2', '.':'q6'},
+	   'q2':{':':'q6'},
+	   'q3':{ '0':'q5', '1':'q5', '2':'q5', '3':'q5',  '.' : 'q5' },
+       'q4':{ '.':'q6' },
+       'q5':{ ':' : 'q6' , '.' : 'q6' },
+       'q6':{ '0':'q7', '1':'q7', '2':'q7', '3':'q7', '4':'q7', '5':'q7' },
+       'q7':{ '0':'q8', '1':'q8' ,'2':'q8' ,'3':'q8' ,'4':'q8' ,'5':'q8' ,'6':'q8' , '7':'q8' ,'8':'q8' , '9':'q8' }
      } 
 
 # the dictionary of accepting states and their
 # corresponding token
+ad = { 'q8':'TIME_TOKEN'
+	
+    }
 
-# Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής...
-ad = { 'q5':'TEST_TOKEN',
-       'q7':'TERM_TOKEN',
-       'q10':'LONG_TOKEN'
-     }
-
+#Apodekto inputs 
+print_input_forms()
 
 # get a string from input
 text = input('give some input>')
@@ -69,7 +75,7 @@ while text:	# that is, while len(text)>0
 	token,position = scan(text,td,ad)
 	
 	if token=='ERROR_TOKEN':
-		print('unrecognized input at pos',position+1,'of',text)
+		print('token: ',token,'string:\"' ,text,'\"\n\t unrecognized input at pos',position+1,'-> \"',text[position],'\"')
 		break
 	
 	print("token:",token,"string:",text[:position])
@@ -77,3 +83,4 @@ while text:	# that is, while len(text)>0
 	# remaining text for next scan
 	text = text[position:]
 	
+	 
